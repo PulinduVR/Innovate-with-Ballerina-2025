54dc2730a76085b90c4c928676d3af1beac0fa82
hadithya <hhadithya34@gmail.com>
2024-09-02 00:52:12 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import Hero from "./components/Hero/hero";
+import ScreenTwo from "./components/ScreenTwo/screen-two";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ScreenThree from "./components/ScreenThree/screen-three";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
+  // React.useEffect(() => {
+  //   gsap.to(".hero", {
+  //     scrollTrigger: {
+  //       trigger: ".main-container",
+  //       start: "top top",
+  //       end: "bottom top",
+  //       scrub: true,
+  //     },
+  //     opacity: 0,
+  //     // y: -100,
+  //     ease: "power1.out",
+  //   });
+  // }, []);
+
   return (
-    <div>App</div>
-  )
+    <div className="main-container">
+      <div className="hero">
+        <Hero />
+      </div>
+      <div className="screen-two">
+        <ScreenTwo />
+        <ScreenThree />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
