5ece834c34bab89da4f18a66fa6094876ea91eae
ShehanRajapaksha <shehan.rajapaksha2002@gmail.com>
2024-09-03 20:14:55 +0530

    Added Screen 03: screen 03 + animation bug fixes + minor updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React,{useRef} from "react";
 import Hero from "./components/Hero/hero";
 import ScreenTwo from "./components/ScreenTwo/screen-two";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import ScreenThree from "./components/ScreenThree/screen-three";
 import View from './views/FaqView';
+import TimeLineView from "./views/TimeLineView";
 
 gsap.registerPlugin(ScrollTrigger);
 
 function App() {
+
+  const mainContainerRef = useRef(null);
   // React.useEffect(() => {
   //   gsap.to(".hero", {
   //     scrollTrigger: {
@@ -24,13 +27,16 @@ function App() {
   // }, []);
 
   return (
-    <div className="main-container">
+    <div className="main-container" ref={mainContainerRef}>
       <div className="hero">
         <Hero />
       </div>
       <div className="screen-two">
         <ScreenTwo />
         <ScreenThree />
+      </div>
+      <div className="screen-three">
+      <TimeLineView parentRef={mainContainerRef} />
       </div>
       <div className="App">
         <View />
