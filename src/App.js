e0d12eb2ddae5dab4393b715b3ed287ee117961e
Shehan Rajapaksha <126370980+ShehanRajapaksha@users.noreply.github.com>
2024-09-05 22:10:46 +0530

    Update App.js
    App.js clear fix

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Hero from "./components/Hero/hero";
+import ScreenTwo from "./components/ScreenTwo/screen-two";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ScreenThree from "./components/ScreenThree/screen-three";
+import View from './views/FaqView';
+import TimeLineView from './views/TimeLineView';
+gsap.registerPlugin(ScrollTrigger);
+
+function App() {
+  // React.useEffect(() => {
+  //   gsap.to(".hero", {
+  //     scrollTrigger: {
+  //       trigger: ".main-container",
+  //       start: "top top",
+  //       end: "bottom top",
+  //       scrub: true,
+  //     },
+  //     opacity: 0,
+  //     // y: -100,
+  //     ease: "power1.out",
+  //   });
+  // }, []);
+
+  return (
+    <div className="main-container">
+      <div className="hero">
+        <Hero />
+      </div>
+      <div className="screen-two">
+        <ScreenTwo />
+        <ScreenThree />
+      </div>
+      <div className="screen-three">
+      <TimeLineView/>
+    </div>
+      <div className="App">
+        <View />
+      </div>
+    </div>
+  );
+}
+
+export default App;
