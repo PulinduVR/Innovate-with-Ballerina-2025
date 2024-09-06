3457388044b669b73ec709acb6cae0030a5ab15e
hadithya <hhadithya34@gmail.com>
2024-09-07 01:11:33 +0530

    Prize-Section Partially Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import PrizeSection from "./components/PrizeSection";
+import React from "react";
+import Hero from "./components/Hero/hero";
+import ScreenTwo from "./components/ScreenTwo/screen-two";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ScreenThree from "./components/ScreenThree/screen-three";
+import View from './views/FaqView';
+import TimeLineView from "./views/TimeLineView";
+import PrizeSection from "../src/components/PrizeSection";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
+
+  // React.useEffect(() => {
+  //   gsap.to(".hero", {
+  //     scrollTrigger: {
+  //       trigger: ".main-container",
+  //       start: "top top",
+  //       end: "bottom top",
+  //       scrub: true,
+  //     },
+  //     opacity: 0,
+  //     // y: -100,
+  //     ease: "power1.out",
+  //   });
+  // }, []);
+
   return (
-    <div className = "main-container">
-      <PrizeSection />
+    <div className="main-container">
+      <div className="hero">
+        <Hero />
+      </div>
+      <div className="screen-two">
+        <ScreenTwo />
+        <ScreenThree />
+      </div>
+      <div>
+        <PrizeSection />
+      </div>
+      <div className="screen-three">
+      <TimeLineView/>
+    </div>
+
+      <div className="App">
+        <View />
+      </div>
     </div>
   );
 }
