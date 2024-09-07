3829351b43116f594613c7c5a45fdebc4b9c01e5
hadithya <hhadithya34@gmail.com>
2024-09-07 08:26:29 +0530

    UPDATED section

--- src/components/PrizeSection.js
+++ src/components/PrizeSection.js
@@ -27,35 +27,43 @@ const PrizeSection = () => {
         <span className="prizes">Prizes</span>
       </div>
 
-      <div className="prize-set">
-        <div className="card-2">
-          <div className="prize-box second-place">
-            <img src="/2nd.png" alt="Second Place" />
-            <div className="prize-info">
-              <h3>2nd Place</h3>
-              <p>Rs 100,000/-</p>
+      <div className='prize-line'>
+        <div className="prize-set">
+          <div className="card-2">
+            <div className="prize-box second-place">
+              <img src="/2nd.png" alt="Second Place" />
+              <div className="prize-info">
+                <h3>2nd Place</h3>
+                <p>Rs 100,000/-</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="card-1">
+            <div className="prize-box first-place">
+              <div className="prize-info">
+                <h3>1st Place</h3>
+                <p>Rs 150,000/-</p>
+              </div>
+              <img src="/1st.png" alt="First Place" style={{ height: "410px" }} />
+            </div>
+          </div>
+
+          <div className="card-3">
+            <div className="prize-box third-place">
+              <img src="/3rd.png" alt="Third Place" />
+              <div className="prize-info">
+                <h3>3rd Place</h3>
+                <p>Rs 75,000/-</p>
+              </div>
             </div>
           </div>
         </div>
-
-        <div className="card-1">
-          <div className="prize-box first-place">
-            <div className="prize-info">
-              <h3>1st Place</h3>
-              <p>Rs 150,000/-</p>
-            </div>
-            <img src="/1st.png" alt="First Place" style={{ height: "370px" }} />
-          </div>
-        </div>
-
-        <div className="card-3">
-          <div className="prize-box third-place">
-            <img src="/3rd.png" alt="Third Place" />
-            <div className="prize-info">
-              <h3>3rd Place</h3>
-              <p>Rs 75,000/-</p>
-            </div>
-          </div>
+        <div className='additional-prizes'>
+          <img src="/p-star.svg" alt="star" />
+          <span>Additonal Prices of <br />Rs 10 000 /- Each </span>
+          <span>for places 4th to 10th</span>
+          <span>Valuable Certificates <br/>for every submission </span>
         </div>
       </div>
       
