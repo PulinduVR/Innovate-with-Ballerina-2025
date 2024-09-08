d32264886faa80b7648ab6392130d4e88d91f003
hadithya <hhadithya34@gmail.com>
2024-09-08 09:04:51 +0530

    link-problem solved

--- src/views/TimeLineView.js
+++ src/views/TimeLineView.js
@@ -282,8 +282,8 @@ export default function TimeLineView() {
                         </p>
                     </div>
                     <div className='buttonSection'>
-                        <button className='button-filled button-filled-text' href='https://forms.gle/uB1ScvWHJYsQbTBs5'> Register Now</button>
-                        <button className='button-outlined button-outlined-text' href="https://drive.google.com/file/d/1PU3KlQlWEZaPHcD0jbWcEpEwVleT9uRF/view?usp=sharing">Download Booklet</button>
+                        <button className='button-filled button-filled-text' onClick={() => window.location.href='https://forms.gle/uB1ScvWHJYsQbTBs5'}>Register Now</button>
+                        <button className='button-outlined button-outlined-text' onClick={() => window.location.href='https://drive.google.com/file/d/1PU3KlQlWEZaPHcD0jbWcEpEwVleT9uRF/view?usp=sharing'}>Download Booklet</button>
                     </div>
                     <div className='svg2'>
                         <svg className='svg-2' xmlns="http://www.w3.org/2000/svg" width="316" height="306" viewBox="0 0 316 306" fill="none">
