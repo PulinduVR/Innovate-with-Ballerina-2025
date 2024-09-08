0edcf44a735d94e4b8d26cfbc84009634c99412f
Himindu Kularathne <143618014+Himindu-Kularathne@users.noreply.github.com>
2024-09-08 22:04:37 +0530

    changes of badges files

--- src/components/Badges/badges.js
+++ src/components/Badges/badges.js
@@ -3,10 +3,6 @@ import './badge.css';
 const Badges = () => {
       return (
          <div className = "main-badge-container">
-
-              
-
-
             <div className = 'center-badge-container'>
                <div className = 'badge-container'>
                   <img src = './ieee-logo.png' alt = "badge1" id = "badge1"></img>
@@ -17,7 +13,6 @@ const Badges = () => {
                <div className = 'badge-container'>
                   <img src = './wso2-logo.webp' alt = "badge3" id = "badge3"></img>
                   
-
                </div>
                <div className = 'badge-container'>
                   <img src = './b-logo.webp' alt = "badge4" id = "badge4"></img>
