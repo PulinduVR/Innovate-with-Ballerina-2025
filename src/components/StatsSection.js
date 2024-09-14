0e6613924c8bfc8c54f37077424de83ed80b89f6
Harindu Hadithya <105522342+hhadithya@users.noreply.github.com>
2024-09-15 00:10:56 +0530

    Update StatsSection.js

--- src/components/StatsSection.js
+++ src/components/StatsSection.js
@@ -19,13 +19,13 @@ const StatsSection = () => {
       <div className="stat-boundary">
         <div className="stat stat-1">
           <h1 className="stat-number">
-            {startCounting && <CountUp end={976} duration={2.5} />}
+            {startCounting && <CountUp end={1015} duration={2.5} />}
           </h1>
           <p className="stat-label">DELEGATES</p>
         </div>
         <div className="stat stat-2">
           <h1 className="stat-number">
-            {startCounting && <CountUp end={261} duration={2.5} />}
+            {startCounting && <CountUp end={272} duration={2.5} />}
           </h1>
           <p className="stat-label">TEAMS</p>
         </div>
