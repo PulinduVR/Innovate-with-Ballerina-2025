1ee50428e76ab2821bd61bd3bd181f8d17040ba2
Jegatheesan Kavienan <kavienanj@gmail.com>
2025-05-17 22:39:08 +0530

    refactor: Update badge layout and styling for improved spacing and organization

--- src/components/Badges/badges.js
+++ src/components/Badges/badges.js
@@ -4,26 +4,37 @@ const Badges = () => {
       return (
          <div className = "main-badge-container">
             <div className = 'center-badge-container'>
-               <div className = 'badge-container'>
-                  <img src = './ieee-logo.png' alt = "badge1" id = "badge1"></img>
+               <div className = 'badge-group-left'>
+                  <h3 className = 'badge-title'>Organised By</h3>
+                  <div className = 'badge-logos'>
+                     <div className = 'badge-container'>
+                        <img src = './ieee-logo.png' alt = "badge1" id = "badge1"></img>
+                     </div>
+                     <div className = 'badge-container'>
+                        <img src = './CS-logo.webp' alt = "badge2" id = "badge2"></img>
+                     </div>
+                  </div>
                </div>
-               <div className = 'badge-container'>
-                  <img src = './CS-logo.webp' alt = "badge2" id = "badge2"></img>
+               
+               <div className = 'divider'></div>
+               
+               <div className = 'badge-group-right'>
+                  <h3 className = 'badge-title'>Powered By</h3>
+                  <div className = 'badge-logos'>
+                     <div className = 'badge-container'>
+                        <img src = './wso2-logo.webp' alt = "badge3" id = "badge3"></img>
+                     </div>
+                     <div className = 'badge-container'>
+                        <img src = './b-logo.webp' alt = "badge4" id = "badge4"></img>
+                     </div>
+                  </div>
                </div>
-               <div className = 'badge-container'>
-                  <img src = './wso2-logo.webp' alt = "badge3" id = "badge3"></img>
-                  
-               </div>
-               <div className = 'badge-container'>
-                  <img src = './b-logo.webp' alt = "badge4" id = "badge4"></img>
-               </div>
-
             </div>
             <div className = 'center-text'>
                <p>Innovation Starts with a Single Line of Code<br />
                <span style={{
                   fontSize: '0.7rem',
-               }}>©2024 Innovate with Ballerina</span></p>
+               }}>©2025 Innovate with Ballerina</span></p>
             </div>
          </div>
       )
