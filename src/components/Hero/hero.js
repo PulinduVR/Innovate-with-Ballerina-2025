e07cd38ee160172c351bd113b1d0e6ff8476dfc6
sahanweerasiri <smudunlahiru@gmail.com>
2025-05-17 09:02:24 +0530

    feat: Add hero section image and improve animations
    This commit introduces the following changes:
    
    - Added `.vscode/` to `.gitignore` to prevent IDE-specific files from being tracked.
    - Added a new image asset `girl.png` to the `src/assets` directory.
    - Updated the Hero component (`src/components/Hero/hero.js`) to:
      - Import the `girl.png` image.
      - Use the imported image in the `<img>` tag for the hero section.
      - Adjusted GSAP animation targetting to correct errors
    
    These changes enhance the visual appeal and user experience of the hero section by adding a relevant image and improving the animation effects.

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -3,6 +3,7 @@ import ScrollDownButton from '../ScrollButton/scrolldown.button';
 import gsap from 'gsap';
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import girl_image from '../../assets/girl.png';
 
 gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
@@ -10,11 +11,11 @@ gsap.registerPlugin(ScrollTrigger);
 function Hero() {
 
   useGSAP(() => {
-    gsap.to("#yellow-text-right", {duration: 2, x: 160});
-    gsap.to("#yellow-text-left", {duration: 2, x: -80});
-    gsap.to("#girl-image", {duration: 2, scale: 1.3});
+    gsap.to("#yellow-text-right", { duration: 2, x: 160 });
+    gsap.to("#yellow-text-left", { duration: 2, x: -80 });
+    gsap.to("#girl-image", { duration: 2, scale: 1.3 });
 
-    gsap.from("#yellow-text-right #yello-text-left", { duration: 2, opacity: 0,  });
+    gsap.from("#yellow-text-right #yello-text-left", { duration: 2, opacity: 0, });
 
   });
 
@@ -23,19 +24,19 @@ function Hero() {
       <div className="hero-container">
         <div className="section-one">
           <p id="yellow-text-left">INNOVATE</p>
-          <img src='./girl.png' alt="girl" id="girl-image"></img>
+          <img src={girl_image} alt="girl" id="girl-image"></img>
           <p id="yellow-text-right">WITH BALLERINA</p>
         </div>
       </div>
-      <div className = "scroll-button-container">
-      <ScrollDownButton className="scroll-button" />
+      <div className="scroll-button-container">
+        <ScrollDownButton className="scroll-button" />
       </div>
       <div className="logo-container">
         <img src='./ieee-logo-min.png' alt="ieee-logo" id="ieee-logo"></img>
         <img src='./CS-logo.webp' alt="cs-logo" id="cs-logo"></img>
         <img src='./wso2-logo.webp' alt="wso2-logo" id="wso2-logo"></img>
         <img src='./b-logo.webp' alt="b-logo" id="balerina-logo"></img>
-        
+
       </div>
 
       <div className='partner'>
