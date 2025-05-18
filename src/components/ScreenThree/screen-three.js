8c2471b4e78e8c8b0d035d2bfce5fb78347a8ff9
sahanweerasiri <smudunlahiru@gmail.com>
2025-05-18 20:10:41 +0530

    Fix: Import useEffect directly from react
    The previous code was importing `React` and then destructuring `useEffect` from it. This commit changes the import to directly import `useEffect` from `react` which is a more concise and direct way to achieve the same result.

--- src/components/ScreenThree/screen-three.js
+++ src/components/ScreenThree/screen-three.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import "./screen-three.css";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
